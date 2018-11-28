2cefe253290a6cb6f0a0f44be6ce43568739d4cd
Moritz Zimmermann <moritz@auterion.com>
2018-11-28 10:30:11 +0100

    Add node to store gps and rovio poses and build paths

---
+++ path_broadcaster/src/path_broadcaster.cpp
@@ -0,0 +1,62 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
+
+class PathBroadcaster {
+  public:
+    PathBroadcaster(ros::NodeHandle& nh);
+    void setup();
+
+  private:
+    ros::NodeHandle& _nh;
+    ros::Subscriber _gps_sub;
+    ros::Subscriber _rovio_sub;
+    ros::Publisher _gps_path_pub;
+    ros::Publisher _rovio_path_pub;
+    nav_msgs::Path _gps_path;
+    nav_msgs::Path _rovio_path;
+
+    void gpsCallback(const geometry_msgs::PoseStamped& msg);
+    void vioCallback(const nav_msgs::Odometry& msg);
+};
+
+PathBroadcaster::PathBroadcaster(ros::NodeHandle& nh) : _nh(nh) {}
+
+void PathBroadcaster::gpsCallback(const geometry_msgs::PoseStamped& msg) {
+  _gps_path.header = msg.header;
+  geometry_msgs::PoseStamped gps_pose = msg;
+
+  _gps_path.poses.push_back(gps_pose);
+  _gps_path_pub.publish(_gps_path);
+}
+
+void PathBroadcaster::vioCallback(const nav_msgs::Odometry& msg) {
+  _rovio_path.header = msg.header;
+  geometry_msgs::PoseStamped vio_pose;
+  vio_pose.pose = msg.pose.pose;
+  vio_pose.header = msg.header;
+
+  _rovio_path.poses.push_back(vio_pose);
+  _rovio_path_pub.publish(_rovio_path);
+}
+
+void PathBroadcaster::setup() {
+  _gps_sub = _nh.subscribe("mavros/local_position/pose", 10, &PathBroadcaster::gpsCallback, this);
+  _rovio_sub = _nh.subscribe("rovio/odometry", 10, &PathBroadcaster::vioCallback, this);  
+
+  _gps_path_pub = _nh.advertise<nav_msgs::Path>("path_gps", 1000);
+  _rovio_path_pub = _nh.advertise<nav_msgs::Path>("path_vio", 1000);
+}   
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "tf_broadcaster");
+
+  ros::NodeHandle nh;
+  
+  PathBroadcaster *frame_aligner = new PathBroadcaster(nh);
+  frame_aligner->setup();
+
+  ros::spin();
+  return 0;
+};
--- tf_broadcaster/src/tf_broadcaster.cpp
+++ tf_broadcaster/src/tf_broadcaster.cpp
@@ -13,27 +13,29 @@ class AlignFrames {
     ros::Subscriber _gps_sub;
     ros::Subscriber _vio_sub;
     bool _offset_initialized;
-    tf::Transform _recent_vio_pose;
+    double _vio_pose_covNorm;
+    tf::Transform _vio_pose;
     tf::Transform _pose_offset;
     tf::TransformBroadcaster _br;
 
     void gpsCallback(const geometry_msgs::PoseStamped& msg);
     void vioCallback(const nav_msgs::Odometry& msg);
+    double l2_norm(std::vector<double> const& v);
 };
 
 AlignFrames::AlignFrames(ros::NodeHandle& nh) : _nh(nh) {
   _offset_initialized = false;
-  _recent_vio_pose.setIdentity();
+  _vio_pose.setIdentity();
   _pose_offset.setIdentity();
 }
 
 void AlignFrames::gpsCallback(const geometry_msgs::PoseStamped& msg) {
-  
-  if(!_offset_initialized) {
+  if(!_offset_initialized && _vio_pose_covNorm > 1.0) {
     tf::Transform gps_pose;
     tf::poseMsgToTF(msg.pose, gps_pose);
-    _pose_offset = _recent_vio_pose.inverse() * gps_pose;
+    _pose_offset = _vio_pose.inverse() * gps_pose;
     _offset_initialized = true;
+    ROS_INFO("initialized GPS VIO offset TF");
   }
 
   _br.sendTransform(tf::StampedTransform(_pose_offset, msg.header.stamp, "world", "map"));
@@ -42,12 +44,25 @@ void AlignFrames::gpsCallback(const geometry_msgs::PoseStamped& msg) {
 void AlignFrames::vioCallback(const nav_msgs::Odometry& msg) {
   tf::Transform vio_pose;
   tf::poseMsgToTF(msg.pose.pose, vio_pose);
-  _recent_vio_pose = vio_pose;
+  _vio_pose = vio_pose;
+
+  auto cov = msg.pose.covariance;
+  std::vector<double> pose_cov(std::begin(cov), std::end(cov));
+
+  _vio_pose_covNorm = l2_norm(pose_cov);
 }
 
 void AlignFrames::setup() {
     _gps_sub = _nh.subscribe("mavros/local_position/pose", 10, &AlignFrames::gpsCallback, this);
     _vio_sub = _nh.subscribe("rovio/odometry", 10, &AlignFrames::vioCallback, this);  
+}
+
+double AlignFrames::l2_norm(std::vector<double> const& v) {
+    double accum = 0.;
+    for (int i = 0; i < v.size(); ++i) {
+        accum += v[i] * v[i];
+    }
+    return sqrt(accum);
 }
 
 int main(int argc, char** argv){
